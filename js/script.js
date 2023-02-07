c20db23b7242d7f43da006f07f12f5d55e63b701
Rahul Khan <khanrahul.1011@gmail.com>
2023-02-07 15:57:33 +0530

    page separation for menu types

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
 // Setting Current Year
 const yearEl = document.querySelector(".year");
 const currentYear = new Date().getFullYear();
-yearEl.textContent = 'Copyright ' + currentYear + ' by Rahul Khan';
+yearEl.textContent = currentYear;
